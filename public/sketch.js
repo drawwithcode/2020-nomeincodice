76612c5a3fb31e837a05d7f6ba35a522404114da
taaaf <mattia.tafel@gmail.com>
2021-01-14 11:47:57 +0100

    k

--- public/sketch.js
+++ public/sketch.js
@@ -1,51 +1,722 @@
-// Create a new connection using socket.io (imported in index.html)
 let socket = io();
 
-// define the function that will be called on a new newConnection
+let mic;
+let sum = 0;
+let totalscore = 0;
+let players = 0;
+let id;
+let prec_totalscore = 0;
+let timer = 0;
+let otherX_players;
+let otherH_players;
+
+let yRatio;
+
+let malus = false;
+
+
+let myOtherPlayers = [];
+
+
+//---------QUANDO SI CONNETTE MANDA L'ID DEL GIOCATORE LOCALE AL SERVER-----------
+
 socket.on("connect", newConnection);
 
 function newConnection() {
   console.log("your id:", socket.id);
+  id = socket.id;
+
+  //quando ti connetti mandi il tuo Id agli altri
+  socket.emit('idPlayerConnected', socket.id);
+
 }
 
-// Define which function should be called when a new message
-// comes from the server with type "mouseBroadcast"
 
-socket.on("mouseBroadcast", otherMouse);
+//----------------PERMESSI IOS--------------
+
+function touchEnded(event) {
+  // check that those functions exist // if they exist it means we are //on iOS and we request the permissions
+  if (DeviceOrientationEvent && DeviceOrientationEvent.requestPermission) {
+    DeviceOrientationEvent.requestPermission()
+  }
+}
+
+
+//----------RIMUOVE L'OGGETTO DEL PLAYER SCOLLEGATO-----------
+
+socket.on("idPlayerDisconnected", removeIdPlayersDisconnected);
+
+function removeIdPlayersDisconnected(idPlayerDisconnected) {
+
+  for (let p = 0; p < myOtherPlayers.length; p++) {
+    if (idPlayerDisconnected === myOtherPlayers[p].getId()) {
+      myOtherPlayers.splice(p, 1);
+    }
+  }
+
+}
+
+
+//----------RICEVE LISTA ID DEGLI ALTRI GIOCATORI, PER OGNUNO CREA UN OGGETTO DELLA CLASSE--------------
+
+socket.on("idPlayerConnectedBroadcast", createOtherPlayer);
+
+function createOtherPlayer(idOtherPlayer) {
+
+  for (let k = 0; k < idOtherPlayer.length; k++) {
+
+    //non crea un doppione del giocatore locale
+    if (idOtherPlayer[k] !== id) {
+      let newPlayer = new OtherPlayer(idOtherPlayer[k], 0, 0);
+      myOtherPlayers.push(newPlayer);
+    }
+
+  }
+
+}
+
+
+//-----------ACQUISISCE INFO DEGLI ALTRI PLAYER E LI ASSEGNA CIASCUNO AD UN ELEMENTO DELLA CLASSE---------
+
+socket.on('micvolume_in', others_micvolume);
+
+function others_micvolume(data) {
+
+  otherX_players = data.x;
+  otherH_players = data.h * windowHeight;
+
+  //riceve i dati info_p e li associa agli Id corrispondenti
+  for (let i = 0; i < myOtherPlayers.length; i++) {
+
+    if (data.id === myOtherPlayers[i].getId()) {
+
+      myOtherPlayers[i].h = otherH_players;
+      myOtherPlayers[i].x = data.x;
+    }
+  }
+
+}
+
+
+//-------RICEVE QUANTI GIOCATORI SONO CONNESSI-----------
+
+socket.on("players", show_players);
+
+function show_players(n_players) {
+  players = n_players;
+  console.log("giocatori connessi: " + players);
+}
+
+
+
+//-----------HIGHSCORE (aggiornato secondo la somma ricevuta dal server)----------
+
+socket.on('highscore', highscore);
+
+function highscore(datahighscore) {
+  totalscore = datahighscore;
+}
+
+
+let bonusServer = false;
+
+socket.on('bonus_effect', bonusEffect);
+
+function bonusEffect(bonusValue) {
+  console.log("dentro funzione bonusEffect");
+  bonusServer = bonusValue;
+}
+
+
+let obstacleX = 0;
+
+let obstacles = [];
+
+socket.on('obstacle_x', createObstacle);
+
+function createObstacle(obstacleXServer) {
+
+  obstacleX = obstacleXServer / 1000 * windowWidth;
+  let newObstacle = new Obstacles(obstacleX);
+  obstacles.push(newObstacle);
+
+}
+
+
+
+let yPlayer;
+
+let starsOne = [];
+let numStarsOne = 60; //quante stelle 1 creare
+
+let starsTwo = [];
+let numStarsTwo = 30; //quante stelle 2 creare
+
+let starsThree = [];
+let numStarsThree = 15; //quante stelle 3 creare
+
+let planet;
+
 
 function setup() {
+
+  frameRate(60);
   createCanvas(windowWidth, windowHeight);
-  background("red");
+
+  //----------AUDIO INPUT, MICROFONO---------
+
+  userStartAudio();
+  // Create an Audio input
+  mic = new p5.AudioIn();
+  // start the Audio Input.  By default, it does not .connect() (to the computer speakers)
+  mic.start();
+
+
+
+  yPlayer = height; // posizione inizale player (parte in basso)
+
+
+
+  //----------CREA LE STELLE-----------
+
+  // for (let p = 0; p < numStarsOne; p++) {
+  //   let newStarOne = new StarsOne();
+  //   starsOne.push(newStarOne);
+  // }
+  //
+  // for (let q = 0; q < numStarsTwo; q++) {
+  //   let newStarTwo = new StarsTwo();
+  //   starsTwo.push(newStarTwo);
+  // }
+  //
+  // for (let r = 0; r < numStarsThree; r++) {
+  //   let newStarThree = new StarsThree();
+  //   starsThree.push(newStarThree);
+  // }
+
+
+
+  for (let p = 0; p < numStarsOne; p++) {
+    let newStarOne = new StarsOne();
+    starsOne.push(newStarOne);
+
+    if (p < numStarsTwo) {
+      let newStarTwo = new StarsTwo();
+      starsTwo.push(newStarTwo);
+    }
+
+    if (p < numStarsThree) {
+      let newStarThree = new StarsThree();
+      starsThree.push(newStarThree);
+    }
+
+  }
+
+
+  planet = new Planets();
+
+
 }
 
-// Callback function called when a new message comes from the server
-// Data parameters will contain the received data
-function otherMouse(data) {
-  console.log("received:", data);
-  noStroke();
-  fill("yellow");
-  ellipse(data.x, data.y, 20);
-}
 
-function mouseDragged() {
-  console.log("sending: ", mouseX, mouseY);
-  noStroke();
-  fill(255);
 
-  // create an object containing the mouse position
-  let message = {
-    x: mouseX,
-    y: mouseY,
-  };
-  // send the object to server,
-  // tag it as "mouse" event
-  socket.emit("mouse", message);
+let maxVol = 0.1;
+let easing = 0.05;
+let calibrationButton = true;
+let button;
+let startCalibration = false;
+let varTimeout;
 
-  ellipse(mouseX, mouseY, 20);
-}
+let vel = 0;
+
+let timerBonus = 0;
+let checkTimer = 0;
+let bonus = false;
+let bonusDuration = 0;
+
+let bx;
+let by;
+let collision = false;
+let d;
+let collisionTimeout;
+
+
 
 function draw() {
-  // evert draw cycle, add a background with low opacity
-  // to create the "fade" effect
-  background(0, 5);
+
+
+  // Get the overall volume (between 0 and 1.0)
+  let vol = mic.getLevel();
+
+  let h = map(vol, 0, maxVol, height, 0);
+
+  let volHighscore = map(vol, 0, maxVol, 0, height);
+
+  h = round(h); //meno numeri dopo la virgola più leggero il codice? forse, boh ci spero
+  volHighscore = round(volHighscore);
+
+
+  //----------EASE PER FLUIDITA' MOVIMENTI-------------
+
+  let targetY = h;
+  let dy = targetY - yPlayer;
+  yPlayer += dy * easing;
+
+
+
+  //----------ROTAZIONE DEL GIROSCOPIO--------------
+
+  const widthY = map(rotationY, -90, 90, 0, width);
+
+
+  //----------VELOCITA' PER SFONDO PARALLASSE--------
+
+  if (prec_totalscore !== 0) {
+    vel = totalscore - prec_totalscore;
+  }
+
+  prec_totalscore = totalscore; //tiene in memoria l'highscore precedente per
+  //ricavare il cambiamento complessivo di volumi di tutti i giocatori
+
+
+  if (!bonus) {
+    background(0);
+  }
+
+
+
+  //--------------BONUSSSS---------------
+
+  if (!startCalibration) {
+
+    let checkBonus = 0;
+
+    checkTimer = 0;
+
+    for (let u = 0; u < myOtherPlayers.length; u++) {
+
+      // console.log(myOtherPlayers[u].h);
+      // console.log(u);
+
+      if (yPlayer < myOtherPlayers[u].h + 100 && yPlayer > myOtherPlayers[u].h - 100) {
+
+        checkBonus++;
+
+      } else {
+        checkTimer++;
+        timerBonus = 0;
+      }
+    }
+
+    if (checkTimer === 0) {
+      timerBonus++;
+    }
+
+    //aggiunto che il bonus si prende solo se si sta più di 200 pixel più in alto dal margine in basso della finestra
+    if (checkBonus === myOtherPlayers.length && checkBonus != 0 && timerBonus === 120 && yPlayer < (height - 200)) {
+      bonus = true;
+      socket.emit('bonus', bonus);
+      console.log("dentro condizioni giuste bonus");
+      bonus = false;
+    }
+
+    if (bonusServer) {
+
+      if (bonusDuration < 50) {
+        background(0, 0, 0, 50);
+        vel += 10000;
+      } else if (bonusDuration < 60 && bonusDuration > 50) {
+
+        background(0, 0, 0, 10);
+        vel += 5000;
+
+      } else {
+        bonusServer = false;
+      }
+
+      bonusDuration++;
+
+    } else {
+      bonusDuration = 0;
+    }
+
+  }
+
+
+  //----------DISPLAY STELLE SFONDO PARALLASSE--------
+
+  if (!startCalibration) {
+
+    for (let p = 0; p < numStarsOne; p++) {
+      starsOne[p].display();
+      starsOne[p].move();
+    }
+
+
+    for (let q = 0; q < numStarsTwo; q++) {
+      starsTwo[q].display();
+      starsTwo[q].move();
+    }
+
+
+    for (let r = 0; r < numStarsThree; r++) {
+      starsThree[r].display();
+      starsThree[r].move();
+    }
+
+  }
+
+  //-------------------DISPLAY PIANETA-------------
+
+  planet.display();
+  planet.move();
+
+
+  //---------------OSTACOLI E COLLISIONI---------------
+
+  bx = widthY;
+  by = yPlayer - 10;
+
+  for (let t = 0; t < obstacles.length; t++) {
+
+    obstacles[t].display();
+    obstacles[t].move();
+
+    d = dist(bx, by, obstacles[t].x, obstacles[t].y);
+
+    if (obstacles[t].y > (height + 15)) { //se l'ostacolo va sotto lo schermo viene tolto dall'array
+      obstacles.splice(t, 1);
+    }
+
+    if (d < 25 && !bonusServer) {
+      console.log("dentro collision 1");
+      collision = true;
+      obstacles.splice(t, 1);
+    }
+  }
+
+
+
+  if (collision) {
+    console.log("dentro collision 2");
+    yPlayer = height;
+    volHighscore = 0;
+    collisionTimeout = setTimeout(resetCollision, 3000);
+  }
+
+
+
+  //---------MOSTRA ALTRI GIOCATORI------------
+  for (let j = 0; j < myOtherPlayers.length; j++) {
+    myOtherPlayers[j].display();
+  }
+
+
+
+  push();
+
+  textAlign(CENTER);
+  fill("yellow");
+  stroke(0);
+
+  text(vel, width / 2, 200);
+
+  text(totalscore, width / 2, 100);
+
+  // text(windowHeight, width / 2, 450);
+  // text(displayHeight, width / 2, 500);
+
+  pop();
+
+  push();
+  fill("yellow");
+  noStroke()
+
+  triangle(widthY - 10, yPlayer, widthY, yPlayer - 30, widthY + 10, yPlayer);
+
+  pop();
+
+  push();
+  fill("white");
+  noStroke();
+
+  triangle(widthY - 5, yPlayer, widthY, yPlayer + random(1, 15), widthY + 5, yPlayer);
+
+  pop();
+
+
+
+  //--------PARAMETRI PASSATI DEL GIOCATORE AL SERVER----------
+
+  yRatio = yPlayer / windowHeight * 100000;
+  yRatio = round(yRatio);
+  yRatio = yRatio / 100000;
+
+
+  let info_p = {
+
+    id: id,
+    h: yRatio,
+    x: widthY,
+    vol: volHighscore
+
+  }
+
+  socket.emit('micvolume', info_p);
+
+
+  //------------CALIBRAZIONE MICROFONO----------------
+
+
+  if (calibrationButton) {
+
+    push();
+
+    fill("salmon");
+    rect(0, 0, width, height);
+
+    if (!button) {
+      button = createButton("Calibra Mic");
+    }
+
+    button.position(width / 2, height / 2);
+
+    button.mousePressed(calibrationMicrophone);
+
+    pop();
+
+  }
+
+  if (startCalibration) {
+    maxVol = max(maxVol, vol);
+    button.remove();
+  }
+
+
+}
+
+
+function resetCollision() {
+  collision = false;
+  clearTimeout(collisionTimeout);
+}
+
+
+function calibrationMicrophone() {
+  startCalibration = true;
+  varTimeout = setTimeout(timerCalibration, 3000);
+}
+
+function timerCalibration() {
+  startCalibration = false;
+  calibrationButton = false;
+}
+
+
+
+
+//-----------CLASSE PER ALTRI GIOCATORI-----------
+
+class OtherPlayer {
+
+  constructor(id, x, h) {
+    this.id = id;
+    this.x = x;
+    this.h = h;
+  }
+
+  display() {
+
+    push();
+    fill(150);
+    noStroke()
+
+    triangle(this.x - 10, this.h, this.x, this.h - 30, this.x + 10, this.h);
+
+    pop();
+
+    push();
+    fill(200);
+    noStroke();
+
+    triangle(this.x - 5, this.h, this.x, this.h + random(1, 15), this.x + 5, this.h);
+
+    pop();
+  }
+
+  getId() {
+    return this.id;
+  }
+
+}
+
+//---------------CLASSE OSTACOLI--------------
+
+class Obstacles {
+
+  constructor(obstacleX) {
+
+    this.r = 30;
+    this.x = obstacleX;
+    this.y = -15;
+
+  }
+
+  display() {
+
+    push();
+    noStroke();
+    fill(255);
+    ellipseMode(CENTER);
+    ellipse(this.x, this.y, this.r);
+    pop();
+
+  }
+
+  move() {
+    this.y += vel / 100;
+  }
+
+}
+
+
+
+
+//-----------CLASSE PER STELLE SFONDO PARALLASSE----------
+
+class StarsOne {
+
+  constructor() {
+
+    this.r = 1;
+    this.x = random(0, width);
+    this.y = random(0, height);
+
+  }
+
+  display() {
+
+    push();
+    noStroke();
+    fill(255, 255, 255, random(50, 255));
+    ellipse(this.x, this.y, this.r, this.r);
+    pop();
+
+  }
+
+  move() {
+
+    if (this.y > height) //if the star goes below the screen
+    {
+      this.y = 0; //reset to the top of the screen
+      this.x = random(0, width);
+    } else {
+      this.y += vel / 3000;
+    }
+  }
+}
+
+
+
+
+
+class StarsTwo {
+
+  constructor() {
+
+    this.r = 2;
+    this.x = random(0, width);
+    this.y = random(0, height);
+
+  }
+
+  display() {
+
+    push();
+    noStroke();
+    fill(255, 255, 255, random(130, 255));
+    ellipse(this.x, this.y, this.r, this.r);
+    pop();
+
+  }
+
+  move() {
+
+    if (this.y > height + 1) //if the star goes below the screen
+    {
+      this.y = 0; //reset to the top of the screen
+      this.x = random(0, width);
+
+    } else {
+      this.y += vel / 1000;
+    }
+  }
+}
+
+
+
+
+class StarsThree {
+
+  constructor() {
+
+    this.r = 4;
+    this.x = random(0, width);
+    this.y = random(0, height);
+  }
+
+  display() {
+
+    push();
+    noStroke();
+    fill(255, 255, 255, random(130, 255));
+    ellipse(this.x, this.y, this.r, this.r);
+    pop();
+
+  }
+
+  move() {
+
+    if (this.y > height + 2) //if the star goes below the screen
+    {
+      this.y = 0; //reset to the top of the screen
+      this.x = random(0, width);
+      // console.log("y 2 " + this.y);
+    } else {
+      this.y += vel / 400;
+    }
+  }
+}
+
+
+
+class Planets {
+
+  constructor() {
+
+    this.r = 300;
+    this.x = random(-2000, width + 2000);
+    this.y = random(0, height);
+  }
+
+  display() {
+
+    push();
+    noStroke();
+    fill(113, 189, 192);
+    ellipse(this.x, this.y, this.r);
+    pop();
+
+  }
+
+  move() {
+
+    if (this.y > height + 150) //if the star goes below the screen
+    {
+      this.y = -150; //reset to the top of the screen
+      this.x = random(-2000, width + 2000);
+    } else {
+      this.y += vel / 200;
+    }
+  }
 }
--- server.js
+++ server.js
@@ -1,37 +1,121 @@
-// load express library
-let express = require("express");
-// create the app
+let express = require('express');
+
 let app = express();
-// define the port where client files will be provided
-let port = process.env.PORT || 3000;
-// start to listen to that port
+
+let highscore = 0;
+
+let port = process.env.PORT || 80;
+
 let server = app.listen(port);
-// provide static access to the files
-// in the "public" folder
-app.use(express.static("public"));
-// load socket library
-let socket = require("socket.io");
-// create a socket connection
+
+app.use(express.static('public'));
+
+let socket = require('socket.io');
+
 let io = socket(server);
-// define which function should be called
-// when a new connection is opened from client
-io.on("connection", newConnection);
-// callback function: the paramenter (in this case socket)
-// will contain all the information on the new connection
+
+io.on('connection', newConnection);
+
+let players = 0;
+
+let d_player = false;
+
+let id_players = [];
+
+let id_player_disconnected;
+
+
+// require('events').EventEmitter.prototype._maxListeners = 1000000;
+
+
+
 function newConnection(socket) {
-  //when a new connection is created, print its id
-  console.log("socket:", socket.id);
 
-  //define what to do on different kind of messages
-  socket.on("mouse", mouseMessage);
 
-  // create the mouseMessage function
-  function mouseMessage(data) {
-    // send the data to all the other clients
-    socket.broadcast.emit("mouseBroadcast", data);
-    // log the sent data
-    console.log(socket.id, data);
+
+  players++; //per ogni connessione aggiungo un giocatore
+
+  io.sockets.emit("players", players);
+  console.log('players:', players);
+
+
+  socket.on("idPlayerConnected", broadcastId);
+
+  //riceve l'Id e lo inserisce nell'array
+  function broadcastId(idPlayerConnected) {
+
+    console.log("id da client :  " + idPlayerConnected);
+
+    id_players.push(idPlayerConnected);
+
+    //il server manda a tutti la lista degli Id di tutti i giocatori connessi
+    io.sockets.emit('idPlayerConnectedBroadcast', id_players);
+
   }
+
+
+  console.log('new connection:', socket.client.id);
+
+  //quando un giocatore si disconnette manda a tutti l'Id
+  socket.on('disconnect', function() {
+    d_player = true;
+    io.sockets.emit("idPlayerDisconnected", socket.id);
+    id_player_disconnected = socket.id;
+
+
+    if (d_player) {
+
+      players--;
+      console.log('players:', players);
+      d_player = false;
+
+      io.sockets.emit("players", players);
+
+      //rimuove l'Id dall'array
+      for (let i = 0; i < id_players.length; i++) {
+        if (id_player_disconnected === id_players[i]) {
+          console.log("player disconnesso " + id_players[i]);
+          id_players.splice(i, 1);
+        }
+      }
+
+    } // dato che la disconnessione dura più di un tick questo if esterno
+    //serve  a non decrementare "players" più di una volta per ogni disconnect
+
+
+  }); //per capire se un giocatore si disconnette; il paramentro d_player serve
+  //a mantenere in memoria il fatto che qualcuno si sia disconnesso
+
+
+
+  socket.on('micvolume', micvolume_message);
+
+  function micvolume_message(dataReceived) {
+
+    highscore += (dataReceived.vol);
+
+    socket.broadcast.emit('micvolume_in', dataReceived);
+
+    io.sockets.emit('highscore', highscore);
+
+  }
+
+
+  socket.on('bonus', function(bonus_value) {
+
+    io.sockets.emit("bonus_effect", bonus_value);
+
+  });
+
+
 }
 
-console.log("node server is running");
+
+let myInterval = setInterval(send_obstacle_x, 500);
+
+function send_obstacle_x(){
+
+  let numRandom = Math.floor((Math.random() * 1000) + 1);
+  io.sockets.emit("obstacle_x", numRandom);
+
+}
